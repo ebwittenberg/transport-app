0e05798ad06cf4041c2b1c5c9559f1f277230008
Eric Wittenberg <ebwittenberg@gmail.com>
2019-05-01 15:49:14 -0400

    sets up file structure for front-end

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="nav">
+        <Link to="/">Home</Link>
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
